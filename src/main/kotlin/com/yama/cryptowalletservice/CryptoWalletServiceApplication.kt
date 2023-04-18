84c47de3e1ac6d19bb54128036ee9ea8647f2544
Tagir Faizullin <t.fayzullin@just-ai.com>
2023-04-18 22:46:25 +0300

    feat: create base service

---
+++ src/main/kotlin/com/yama/cryptowalletservice/CryptoWalletServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.yama.cryptowalletservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoWalletServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CryptoWalletServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/yama/cryptowalletservice/CryptoWalletServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.yama.cryptowalletservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoWalletServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
